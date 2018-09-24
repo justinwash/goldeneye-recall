b97ff4261018527428ff0c651bd6c32243897056
Wash <washj@stifel.com>
2018-09-24 10:39:14 -0500

    renaming things

--- Program.cs
+++ Program.cs
@@ -6,60 +6,54 @@ using System.Text;
 
 namespace GoldenEyeRecall
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var playing = true;
+      while (true)
+      {
+        var p = new ScreenReader();
+        if (p.IsLoading("obs64", 600, 200))
         {
-            var paused = false;
-            var playing = true;
-            while (true)
-            {
-                var p = new Pixel();
-                if (p.ShouldPlay("obs64", 600, 200))
-                {
-                    if (!playing)
-                    {
-                        PlayVid();
-                        playing = true;
-                        paused = false;
-                    }
-                }
-                else
-                {
-                    if (!paused)
-                    {
-                        ResetVid();
-                        paused = true;
-                        playing = false;
-                    }
-                }
-
-                Thread.Sleep(400);
-            }
+          if (!playing)
+          {
+            PlayVid();
+            playing = true;
+          }
+        }
+        else if (playing)
+        {
+          ResetVid();
+          playing = false;
         }
 
-        static async void PlayVid()
-        {
-            string commandUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_play";
-            var byteArray = Encoding.ASCII.GetBytes(":terryjr");
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-                HttpResponseMessage res = await client.GetAsync(commandUrl);
-            }
-        }
-
-        static async void ResetVid()
-        {
-            string pauseUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_pause";
-            string resetUrl = "http://127.0.0.1:8080/requests/status.xml?command=seek&val=0";
-            var byteArray = Encoding.ASCII.GetBytes(":terryjr");
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-                HttpResponseMessage res = await client.GetAsync(pauseUrl);
-                HttpResponseMessage res2 = await client.GetAsync(resetUrl);
-            }
-        }
+        Thread.Sleep(80);
+      }
     }
+
+    static async void PlayVid()
+    {
+      string commandUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_play";
+      var byteArray = Encoding.ASCII.GetBytes(":terryjr");
+      using (HttpClient client = new HttpClient())
+      {
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        HttpResponseMessage res = await client.GetAsync(commandUrl);
+      }
+    }
+
+    static async void ResetVid()
+    {
+      string pauseUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_pause";
+      string resetUrl = "http://127.0.0.1:8080/requests/status.xml?command=seek&val=0";
+      var byteArray = Encoding.ASCII.GetBytes(":terryjr");
+      using (HttpClient client = new HttpClient())
+      {
+        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        HttpResponseMessage res = await client.GetAsync(pauseUrl);
+        HttpResponseMessage res2 = await client.GetAsync(resetUrl);
+      }
+    }
+  }
 }
