6c92b60fe925aea1237adcd46fd0db45619e9edc
Justin Wash <trifectuh@icloud.com>
2018-10-01 20:19:59 -0700

    obs32 compatibility, less crashes (hopefully)

--- Program.cs
+++ Program.cs
@@ -6,53 +6,74 @@ using System.Text;
 
 namespace GoldenEyeRecall
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var p = new ScreenReader();
-            var playing = false;
-            while (true)
-            {
-                if (!p.IsLoading())
-                {
-                    PlayVid();
-                    playing = true;
-                }
-                else
-                {
-                    if (playing)
-                    {
-                        ResetVid();
-                        playing = false;
-                    }
-                }
-                Thread.Sleep(28);
-            }
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new ScreenReader();
+			var playing = false;
+			while (ScreenReader.proc == null)
+			{
+				p.GetObsInstance();
+			}
 
-        static async void PlayVid()
-        {
-            string commandUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_play";
-            var byteArray = Encoding.ASCII.GetBytes(":terryjr");
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-                HttpResponseMessage res = await client.GetAsync(commandUrl);
-            }
-        }
+			while (true)
+			{
+				if (!p.IsLoading())
+				{
+					PlayVid();
+					playing = true;
+				}
+				else
+				{
+					if (playing)
+					{
+						ResetVid();
+						playing = false;
+					}
+				}
+				Thread.Sleep(14);
+			}
+		}
 
-        static async void ResetVid()
-        {
-            string pauseUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_pause";
-            string resetUrl = "http://127.0.0.1:8080/requests/status.xml?command=seek&val=0";
-            var byteArray = Encoding.ASCII.GetBytes(":terryjr");
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-                HttpResponseMessage res = await client.GetAsync(pauseUrl);
-                HttpResponseMessage res2 = await client.GetAsync(resetUrl);
-            }
-        }
-    }
+		static async void PlayVid()
+		{
+			string commandUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_play";
+			var byteArray = Encoding.ASCII.GetBytes(":terryjr");
+			using (HttpClient client = new HttpClient())
+			{
+				client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+				try
+				{
+					HttpResponseMessage res = await client.GetAsync(commandUrl);
+				}
+				catch
+				{
+					Console.WriteLine("Cannot connect to VLC over HTTP");
+					Thread.Sleep(10000);
+				}
+			}
+		}
+
+		static async void ResetVid()
+		{
+			string pauseUrl = "http://127.0.0.1:8080/requests/status.xml?command=pl_pause";
+			string resetUrl = "http://127.0.0.1:8080/requests/status.xml?command=seek&val=0";
+			var byteArray = Encoding.ASCII.GetBytes(":terryjr");
+			using (HttpClient client = new HttpClient())
+			{
+				client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+				try
+				{
+					HttpResponseMessage res = await client.GetAsync(pauseUrl);
+					HttpResponseMessage res2 = await client.GetAsync(resetUrl);
+				}
+				catch
+				{
+					Console.WriteLine("Cannot connect to VLC over HTTP");
+					Thread.Sleep(10000);
+				}
+			}
+		}
+	}
 }
--- Screen.cs
+++ Screen.cs
@@ -9,87 +9,111 @@ using System.Threading;
 
 namespace Screen
 {
-    public class ScreenReader
-    {
-        private List<byte[]> buffer = new List<byte[]>();
-        Process proc = Process.GetProcessesByName("obs64")[0];
-        User32.Rect rect = new User32.Rect();
+	public class ScreenReader
+	{
+		private List<byte[]> buffer = new List<byte[]>();
+		public static Process proc;
+		User32.Rect rect = new User32.Rect();
 
-        public void UpdateBuffer(byte[] data)
-        {
-            if (buffer.Count >= 6)
-            {
-                buffer.Remove(buffer[0]);
-            }
-            buffer.Add(data);
-        }
+		public void GetObsInstance()
+		{
+			if (Process.GetProcessesByName("obs64").Count() > 0)
+			{
+				Console.WriteLine("found obs64 instance!");
+				proc = Process.GetProcessesByName("obs64")[0];
+			}
+			else if (Process.GetProcessesByName("obs32").Count() > 0)
+			{
+				Console.WriteLine("found obs32 instance!");
+				proc = Process.GetProcessesByName("obs32")[0];
+			}
+			else
+			{
+				Console.WriteLine("Cannot find OBS instance");
+				Thread.Sleep(1000);
+			}
+		}
 
-        public byte[] CaptureTestArea()
-        {
-            User32.GetWindowRect(proc.MainWindowHandle, ref rect);
-            int width = rect.right - rect.left;
-            int height = rect.bottom - rect.top;
+		public void UpdateBuffer(byte[] data)
+		{
+			if (buffer.Count >= 2)
+			{
+				buffer.Remove(buffer[0]);
+			}
+			buffer.Add(data);
+		}
 
-            var bmp = new Bitmap(48, 24, PixelFormat.Format24bppRgb);
-            Graphics graphics = Graphics.FromImage(bmp);
-            graphics.CopyFromScreen(rect.left + width / 2, rect.top + height / 4, 0, 0, new Size(48, 24), CopyPixelOperation.SourceCopy);
-            //bmp.Save("pic.png", ImageFormat.Png);
-            var testRect = new Rectangle(0, 0, 48, 24);
-            BitmapData bmpData = bmp.LockBits(testRect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
-            IntPtr ptr = bmpData.Scan0;
-            int bytes = Math.Abs(bmpData.Stride) * bmpData.Height;
-            byte[] rgbValues = new byte[bytes];
+		public byte[] CaptureTestArea()
+		{
+			if (proc != null)
+			{
+				proc.Refresh();
+				User32.GetWindowRect(proc.MainWindowHandle, ref rect);
+				int width = rect.right - rect.left;
+				int height = rect.bottom - rect.top;
 
-            // Copy the RGB values into the array.
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-            return rgbValues;
-        }
+				var bmp = new Bitmap(48, 24, PixelFormat.Format24bppRgb);
+				Graphics graphics = Graphics.FromImage(bmp);
+				graphics.CopyFromScreen(rect.left + width / 2, rect.top + height / 4, 0, 0, new Size(48, 24), CopyPixelOperation.SourceCopy);
+				//bmp.Save("pic.png", ImageFormat.Png);
+				var testRect = new Rectangle(0, 0, 48, 24);
+				BitmapData bmpData = bmp.LockBits(testRect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+				IntPtr ptr = bmpData.Scan0;
+				int bytes = Math.Abs(bmpData.Stride) * bmpData.Height;
+				byte[] rgbValues = new byte[bytes];
 
-        public bool IsLoading()
-        {
-            var colorData = CaptureTestArea();
-            UpdateBuffer(colorData);
+				// Copy the RGB values into the array.
+				System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+				return rgbValues;
+			}
+			return new byte[0];
+		}
 
-            List<bool> blackBuffer = new List<bool>();
-            foreach (var b in buffer)
-            {
-                var avg = b.Average(x => int.Parse(x.ToString()));
-                if (avg > 10)
-                {
-                    Console.Write("\r{0}%   ", avg);
-                    //buffer.Clear();
-                    blackBuffer.Add(false);
-                }
-                else
-                {
-                    Console.Write("\r{0}%   ", avg);
-                    blackBuffer.Add(true);
-                }
-            }
+		public bool IsLoading()
+		{
+			var colorData = CaptureTestArea();
+			UpdateBuffer(colorData);
 
-            if (blackBuffer.All(t => t == true))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+			List<bool> blackBuffer = new List<bool>();
+			foreach (var b in buffer)
+			{
+				var avg = b.Average(x => int.Parse(x.ToString()));
+				if (avg > 10)
+				{
+					Console.Write("\r{0}%   ", avg);
+					//buffer.Clear();
+					blackBuffer.Add(false);
+				}
+				else
+				{
+					Console.Write("\r{0}%   ", avg);
+					blackBuffer.Add(true);
+				}
+			}
 
-        private class User32
-        {
-            [StructLayout(LayoutKind.Sequential)]
-            public struct Rect
-            {
-                public int left;
-                public int top;
-                public int right;
-                public int bottom;
-            }
+			if (blackBuffer.All(t => t == true))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 
-            [DllImport("user32.dll")]
-            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
-        }
-    }
+		private class User32
+		{
+			[StructLayout(LayoutKind.Sequential)]
+			public struct Rect
+			{
+				public int left;
+				public int top;
+				public int right;
+				public int bottom;
+			}
+
+			[DllImport("user32.dll")]
+			public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+		}
+	}
 }
